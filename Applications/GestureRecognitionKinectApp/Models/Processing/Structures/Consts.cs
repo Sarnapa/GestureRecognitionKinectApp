e6f6326ba04095bfac21d2b7181c2b126c344112
Sarnapa <piotrak.michal@gmail.com>
2025-02-02 20:48:53 +0100

    Better times limit to get more quality samples

--- Applications/GestureRecognitionKinectApp/Models/Processing/Structures/Consts.cs
+++ Applications/GestureRecognitionKinectApp/Models/Processing/Structures/Consts.cs
@@ -7,15 +7,15 @@ namespace GestureRecognition.Applications.GestureRecognitionKinectApp.Models.Pro
 		public const string GestureRecordFileExtension = ".record";
 		// TODO: This should be set in user options
 		public const float GestureRecordResizingCoef = 0.4f;
-		public const float ColorSpaceBodyJointDisplacementPositionLimit = 20f;
+		public const float ColorSpaceBodyJointDisplacementPositionLimit = 25f;
 
 		public static readonly TimeSpan DefaultBodyTrackingStoppedTime = TimeSpan.FromSeconds(3d);
 		public static readonly TimeSpan GestureToStartRecordingTimeLimit = TimeSpan.FromSeconds(3d);
-		public static readonly TimeSpan GestureRecordTimeLimit = TimeSpan.FromSeconds(10d);
-		public static readonly TimeSpan GestureRecordUserWithoutMovementTimeLimit = TimeSpan.FromSeconds(2d);
+		public static readonly TimeSpan GestureRecordTimeLimit = TimeSpan.FromSeconds(3d);
+		public static readonly TimeSpan GestureRecordUserWithoutMovementTimeLimit = TimeSpan.FromSeconds(1d);
 		public static readonly TimeSpan GestureToStartRecognizingTimeLimit = TimeSpan.FromSeconds(3d);
-		public static readonly TimeSpan GestureToRecognizeRecordTimeLimit = TimeSpan.FromSeconds(10d);
-		public static readonly TimeSpan GestureToRecognizeRecordUserWithoutMovementTimeLimit = TimeSpan.FromSeconds(2d);
+		public static readonly TimeSpan GestureToRecognizeRecordTimeLimit = TimeSpan.FromSeconds(3d);
+		public static readonly TimeSpan GestureToRecognizeRecordUserWithoutMovementTimeLimit = TimeSpan.FromSeconds(1d);
 		public static readonly TimeSpan WaitingForRecognizingGestureResultTimeLimit = TimeSpan.FromSeconds(3d);
 	}
 }
