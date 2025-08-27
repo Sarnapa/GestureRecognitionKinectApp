2664e2d12e56e473ce26df66c049a0f01b6b18d7
Sarnapa <piotrak.michal@gmail.com>
2025-08-27 09:04:10 +0200

    Increasing time concerning gesture recording and recognizing

--- Applications/GestureRecognitionKinectApp/Models/Processing/Structures/Consts.cs
+++ Applications/GestureRecognitionKinectApp/Models/Processing/Structures/Consts.cs
@@ -11,11 +11,11 @@ namespace GestureRecognition.Applications.GestureRecognitionKinectApp.Models.Pro
 
 		public static readonly TimeSpan DefaultBodyTrackingStoppedTime = TimeSpan.FromSeconds(3d);
 		public static readonly TimeSpan GestureToStartRecordingTimeLimit = TimeSpan.FromSeconds(3d);
-		public static readonly TimeSpan GestureRecordTimeLimit = TimeSpan.FromSeconds(2.5d);
-		public static readonly TimeSpan GestureRecordUserWithoutMovementTimeLimit = TimeSpan.FromSeconds(0.5d);
+		public static readonly TimeSpan GestureRecordTimeLimit = TimeSpan.FromSeconds(3d);
+		public static readonly TimeSpan GestureRecordUserWithoutMovementTimeLimit = TimeSpan.FromSeconds(1d);
 		public static readonly TimeSpan GestureToStartRecognizingTimeLimit = TimeSpan.FromSeconds(3d);
-		public static readonly TimeSpan GestureToRecognizeRecordTimeLimit = TimeSpan.FromSeconds(2.5d);
-		public static readonly TimeSpan GestureToRecognizeRecordUserWithoutMovementTimeLimit = TimeSpan.FromSeconds(0.5d);
+		public static readonly TimeSpan GestureToRecognizeRecordTimeLimit = TimeSpan.FromSeconds(3d);
+		public static readonly TimeSpan GestureToRecognizeRecordUserWithoutMovementTimeLimit = TimeSpan.FromSeconds(1d);
 		public static readonly TimeSpan WaitingForRecognizingGestureResultTimeLimit = TimeSpan.FromSeconds(3d);
 	}
 }
